db0314414daa79f7433f2944c6bb52f6d9ef141b
micziz <miczicontent@gmail.com>
2022-04-28 10:32:05 +0200

    RUD and add more figlet

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # Import stadndard libraries
-import time, math, random, sys, os
+import time, random, sys, os
 # Import third-party libraries
 from pyfiglet import Figlet
 from password_generator import PasswordGenerator
@@ -52,12 +52,16 @@ def countdown():
     countdown(int(t)) 
 
 def passwordGenerator():
+    os.system(clear_command)
+    print(f.renderText('PASSWORD GENERATOR'))
     pwo = PasswordGenerator()
     password = pwo.generate_password()
     print("Your password is: ", password)
     
 
 def checkLeapYear():
+    os.system(clear_command)
+    print(f.renderText('CHECK LEAP YEAR'))
     year = int(input("Enter a year:- "))   # Here, you take the input from the user
     if(((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)):   # Here, you check if the year is a leap year
         print("{0} is a leap year!!".format(year))  # Here, you print the year
@@ -65,6 +69,8 @@ def checkLeapYear():
         print("{0} is not a leap year!!".format(year))
 
 while True:
+    os.system(clear_command)
+    print(f.renderText('MAIN MENU'))
     pubChoose = input("Hello and welcome to PUB or Python Utility Bot\nSelect an option:")
     if pubChoose == "guess the number":
         guessTheNumber()
